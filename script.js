6f4db29dee03869dac542d12bb9e6f690f764e3a
Md Sarfraj <116196482+mdsarfarj@users.noreply.github.com>
2024-02-11 14:01:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function trailingZeros(n) {
   //your JS code here. If required.
+	let ct=0; 
+	while(n>0){
+		n=Math.floor(n/5);
+		ct+=n;
+	}
+	return ct;
 }
 
 const input = prompt("Enter a number");
